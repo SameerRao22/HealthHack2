6012cbc9df5fe5bfb151a4a07c2c253305fbf835
SameerRao <sameerrao22@mittymonarch.com>
2020-01-26 13:08:55 -0800

    added the gui to master

--- grapher.py
+++ grapher.py
@@ -1,2 +1,39 @@
-import matplotlib.pyplot as plt
+from tkinter import *
+from tkinter import ttk
 
+def clicked():
+    weightNum =  weight_box.get()
+    heightNum = height_box.get()
+    print("weight: " + weightNum, "height: ", heightNum)
+
+window = Tk()
+window.title("Welcome to LikeGeeks app")
+window.geometry("500x500")
+
+tab_control = ttk.Notebook(window)
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+
+tab_control.add(tab1, text='First')
+tab_control.add(tab2, text='Second')
+
+weight_label = Label(tab1, text='Enter your Weight: ')
+weight_label.grid(column=0, row=0)
+weight_box = Entry(tab1, width = 10)
+weight_box.grid(column = 1, row = 0)
+# weight_button = Button(tab1, text = "Enter")
+# weight_button.grid(column=2, row=0)
+
+height_label = Label(tab1, text='Enter your Height: ')
+height_label.grid(column=0, row=1)
+height_box = Entry(tab1, width = 10)
+height_box.grid(column = 1, row = 1)
+# height_button = Button(tab1, text = "Enter")
+# height_button.grid(column=2, row=1)
+
+submit_button = Button(tab1, text = "Submit", command = clicked)
+submit_button.grid(column = 0, row = 5)
+
+tab_control.pack(expand=1, fill='both')
+
+window.mainloop()
