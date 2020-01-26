b954acf5794aee321224aaac1f558e61426adb3f
Krish Prasad <kp.kriish@gmail.com>
2020-01-26 13:06:37 -0800

    updated bmi_display

--- bmi_display.py
+++ bmi_display.py
@@ -6,26 +6,23 @@ from tkinter import messagebox
 
 def enter_height():
     height = entry_1.get()
-
+    string_to_display = "Hello" + height
     label_2 = Label(root)
-    label_2["text"] = height
-    label_2.grid(row=4, column=1)
+    label_2["text"] = string_to_display
+    label_2.grid(row=3, column=1)
 
 
 root = Tk()
 
 label_1 = Label(root, text= "Height: ")
 entry_1 = Entry(root)
-button_1 = Button(root, text="Enter", command=enter_height());
+button_1 = Button(root, text="Enter", command=enter_height);
 
 
 
 label_1.grid(row=0, column=0)
 entry_1.grid(row=0, column=1)
 button_1.grid(row=1, column=0)
-
-
-
 
 
 root.mainloop()
