b2652759682fc5d672135ab330804627a34e3dce
SameerRao <sameerrao22@mittymonarch.com>
2020-01-26 10:25:38 -0800

    .

---
+++ Water_Sensor.py
@@ -0,0 +1,4 @@
+class Water_Sensor:
+    pH = 5
+    def Return_PH(self):
+        return self.pH
---
+++ test.py
@@ -0,0 +1,4 @@
+from Water_Sensor import Water_Sensor
+
+sensor = Water_Sensor()
+print(sensor.Return_PH())
