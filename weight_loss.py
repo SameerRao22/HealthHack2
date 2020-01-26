7cb856a619e26f2ece272ba143489c696befd31e
Krish Prasad <kp.kriish@gmail.com>
2020-01-26 15:02:20 -0800

    started weight_loss

---
+++ weight_loss.py
@@ -0,0 +1,10 @@
+def weight_goal(weight_class, weight, height):
+    if (weight_class == "obese") or (weight_class == "overweight"):
+        target = height**2 * 25
+    elif(weight_class == "underweight"):
+        target = height**2 * 18.5
+
+
+
+
+
