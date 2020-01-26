64b60c620698775ef9b8b26971dd104a086041ce
SameerRao <sameerrao22@mittymonarch.com>
2020-01-26 13:25:56 -0800

    updated BMI class and display

--- bmi_display.py
+++ bmi_display.py
@@ -1,37 +1,40 @@
 from tkinter import *
 from tkinter import ttk
+from BMI_Class import BMI_Class
+from tkinter import messagebox
 
-def clicked():
-    weightNum =  weight_box.get()
-    heightNum = height_box.get()
-    print("weight: " + weightNum, "height: ", heightNum)
+def SubmitClicked():
+    weightNum =  float(weight_box.get())
+    heightNum = float(height_box.get())
+    messagebox.showinfo('BMI CHART', "Your BMI was successfully calculated\n If you want to see the graph switch to tab 2 or enter more values")
+    BMI_Chart.add_BMI(weightNum, heightNum)
 
 window = Tk()
-window.title("Welcome to LikeGeeks app")
+window.title("BMI Chart")
 window.geometry("500x500")
+
+BMI_Chart = BMI_Class()
 
 tab_control = ttk.Notebook(window)
 tab1 = ttk.Frame(tab_control)
 tab2 = ttk.Frame(tab_control)
 
-tab_control.add(tab1, text='First')
-tab_control.add(tab2, text='Second')
+tab_control.add(tab1, text='Input')
+tab_control.add(tab2, text='Graph')
 
-weight_label = Label(tab1, text='Enter your Weight: ')
+weight_label = Label(tab1, text='Enter your Weight(LBS): ')
 weight_label.grid(column=0, row=0)
 weight_box = Entry(tab1, width = 10)
 weight_box.grid(column = 1, row = 0)
-# weight_button = Button(tab1, text = "Enter")
-# weight_button.grid(column=2, row=0)
 
-height_label = Label(tab1, text='Enter your Height: ')
+
+height_label = Label(tab1, text='Enter your Height(IN): ')
 height_label.grid(column=0, row=1)
 height_box = Entry(tab1, width = 10)
 height_box.grid(column = 1, row = 1)
-# height_button = Button(tab1, text = "Enter")
-# height_button.grid(column=2, row=1)
 
-submit_button = Button(tab1, text = "Submit", command = clicked)
+
+submit_button = Button(tab1, text = "Submit", command = SubmitClicked)
 submit_button.grid(column = 0, row = 5)
 
 tab_control.pack(expand=1, fill='both')
